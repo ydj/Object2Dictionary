2d2f359601db4be868b451a13df4354e7fdd53a6
YDJ <nextdj@163.com>
2014-11-08 19:58:49 +0800

    创建工程

---
+++ Object2Dictionary/Header.h
@@ -0,0 +1,16 @@
+//
+//  Header.h
+//  Object2Dictionary
+//
+//  Created by YDJ on 14/11/6.
+//  Copyright (c) 2014年 jingyoutimes. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Header : NSObject
+
+@property (nonatomic,copy)NSString * code;
+@property (nonatomic,copy)NSString * message;
+
+@end
