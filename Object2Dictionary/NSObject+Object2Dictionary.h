ce16b6556b8afe884fe7e194099f1b53470df963
YDJ <nextdj@163.com>
2014-11-21 13:15:43 +0800

    ADD LICENSE MESSAGE

--- Object2Dictionary/NSObject+Object2Dictionary.h
+++ Object2Dictionary/NSObject+Object2Dictionary.h
@@ -2,7 +2,7 @@
 //  NSObject+Object2Dictionary.h
 //  Object2Dictionary
 //
-//  Copyright (c) 2014 Object2Dictionary (https://github.com/ydj/Object2Dictionary)
+//  Copyright (c) 2014 YDJ ( https://github.com/ydj/Object2Dictionary )
 //                                        
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
